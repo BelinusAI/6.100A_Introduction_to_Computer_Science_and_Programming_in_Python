9fcf7346941342f7af57792e413aa6df6ed4a469
Belinus <52972482+BelinusAI@users.noreply.github.com>
2023-07-02 14:27:06 -0500

    Add files via upload

---
+++ ps0/ps0.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 15 09:59:37 2022
+
+@author: Alexander
+"""
+import math
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+
+print("X**y = ", x**y)
+print("log(x) = ", math.log(x, 2))
